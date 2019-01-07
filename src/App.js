ae9cfecfc0bf5e157446cf893e197e01e6e8a077
Davi Viana <davipviana@gmail.com>
2019-01-07 00:14:50 -0200

    Reusable customized input component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import CustomizedInput from './components/CustomizedInput';
 
 class App extends Component {
 
@@ -80,18 +81,9 @@ class App extends Component {
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
               <form className="pure-form pure-form-aligned" onSubmit={this.sendForm}>
-                <div className="pure-control-group">
-                  <label htmlFor="name">Name</label>
-                  <input id="name" type="text" name="name" value={this.state.name} onChange={this.setName} />
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="password">Password</label>
-                  <input id="password" type="password" name="password" value={this.state.password} onChange={this.setPassword}/>
-                </div>
+                <CustomizedInput id="name" type="text" name="name" value={this.state.name} onChange={this.setName} label="Name" />
+                <CustomizedInput id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                <CustomizedInput id="password" type="password" name="password" value={this.state.password} onChange={this.setPassword} label="Password" />
                 <div className="pure-control-group">
                   <label></label>
                   <button type="submit" className="pure-button pure-button-primary">Send</button>
